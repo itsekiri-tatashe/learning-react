01a28b7ed245ff0580f181998773aa4041de5aa9
EyimofeP <eyimofe36@gmail.com>
2025-02-16 18:52:01 +0100

    Saving Form objects using useState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,13 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: "", age: 0 };
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-
     console.log(person);
   };
 
@@ -20,14 +17,30 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input ref={nameRef} type="text" className="form-control" id="name" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          type="text"
+          className="form-control"
+          id="name"
+        />
       </div>
 
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} type="number" className="form-control" id="age" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          value={person.age}
+          type="number"
+          className="form-control"
+          id="age"
+        />
       </div>
 
       <button className="btn btn-primary" type="submit">
