f6639e698669131368ec9d49799c77bf4aac92db
EyimofeP <eyimofe36@gmail.com>
2025-02-22 22:31:29 +0100

    Learnt about fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,26 @@
-import { useState } from "react";
-import ExpenseTable from "./components/expense-project/ExpenseTable";
-import ExpenseForm from "./components/expense-project/ExpenseForm";
-import { Expense } from "./components/expense-project/ExpenseInterfaces";
-import ExpenseFilter from "./components/expense-project/ExpenseFilter";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-// Define a TypeScript type for expense entries
+interface User {
+  id: number;
+  name: string;
+}
 function App() {
-  // Dummy Data
-  const [expenseData, setExpenseData] = useState<Expense[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
 
-  // State for edit, if set to null add a new expense else edit an existing expense
-  const [editingExpense, setEditingExpense] = useState<Expense | null>(null);
-
-
-  // Add or Edit Expense
-  const saveExpense = (expense: any) => {
-    if (editingExpense) {
-      // Update existing expense
-      setExpenseData((prev) =>
-        prev.map((item) => (item.id === editingExpense.id ? { ...expense, id: editingExpense.id } : item))
-      );
-      setEditingExpense(null); // Reset after editing
-    } else {
-      // Add new expense
-      setExpenseData([...expenseData, { ...expense, id: Date.now() }]);
-    }
-  };
-
-  // Edit Expense
-  const editExpense = (expense: Expense) => {
-    setEditingExpense(expense);
-  };
-
-  // Delete Object from List
-  const deleteExpense = (id: number) => {
-    setExpenseData(expenseData.filter((expense) => expense.id !== id));
-  };
-
-  // Filter Table
-  const visbleExpense = selectedCategory
-    ? expenseData.filter((expense) => expense.category === selectedCategory)
-    : expenseData;
-
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data));
+  }, []);
   return (
-    <>
-      <ExpenseForm saveExpense={saveExpense} editingExpense={editingExpense} />
-      <ExpenseFilter
-        selectCategory={(category) => setSelectedCategory(category)}
-      />
-      <ExpenseTable
-        expenseData={visbleExpense}
-        onDelete={deleteExpense}
-        onEdit={editExpense}
-      />
-    </>
+    <ul className="list-group">
+      {users.map((user) => (
+        <li className="list-group-item" key={user.id}>
+          {user.name}
+        </li>
+      ))}
+    </ul>
   );
 }
-
 export default App;
